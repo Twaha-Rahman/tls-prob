2df1623594eb7ea21d793e89ae7993bb25d3879f
Twaha-Rahman <mahi6703890@gmail.com>
2020-05-06 11:37:45 +0600

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const toxicity = require('@tensorflow-models/toxicity');
+
+const threshold = 0.9;
+
+toxicity.load(threshold).then((model) => {
+  const sentences = ['you suck'];
+
+  model.classify(sentences).then((predictions) => {
+    console.log(predictions);
+  });
+});
